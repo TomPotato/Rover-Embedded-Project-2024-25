553c763d81ebae93d70aab4f96cca7f8e511af03
AlessioWu <115952325+AlessioWu@users.noreply.github.com>
2025-02-17 10:13:02 +0100

    Add files via upload

--- Our Code/SideCode/Connection_bluetooth.py
+++ Our Code/SideCode/Connection_bluetooth.py
@@ -1,7 +1,7 @@
 import serial
 
 # COM ports (Check Device Manager)
-msp_port = 'COM9'   # MSP432 Serial
+msp_port = 'COM9'    # MSP432 Serial
 hc05_port = 'COM14'  # HC-05 Bluetooth Outgoing COM
 
 try:
@@ -12,15 +12,22 @@ try:
     print("Relaying joystick commands from MSP432 to Arduino via Bluetooth...")
 
     while True:
+        # MSP to HC05
         if msp.in_waiting:
-            command = msp.readline().decode().strip()  # Read MSP432 data
-            if command in ['F', 'B', 'L', 'R', 'S']:  # Valid commands
-                print(f"{command}")
-                hc05.write(command.encode())  # Send via Bluetooth
+            data_from_MSP = msp.readline().decode().strip()  # Read MSP432 data
+            if data_from_MSP in ['F', 'B', 'L', 'R', 'S', 'W', 'P']:  # Valid commands
+                print(f"Sending:----------{data_from_MSP}----------")
+                hc05.write(data_from_MSP.encode())  # Send via Bluetooth
+
+        # HC05 to MSP
+        if hc05.in_waiting:
+            data_from_hc05 = hc05.readline().decode().strip()
+            print(f"Receiving: {data_from_hc05}")
+            # msp.write(data_from_hc05.encode())
 
 except KeyboardInterrupt:
-    print("\n Stopping relay...")
+    print("\nStopping relay...")
 finally:
     msp.close()
     hc05.close()
-    print("Ports closed.")
+    print("Ports closed.")
